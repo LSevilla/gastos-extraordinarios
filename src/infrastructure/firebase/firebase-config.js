e870f440a319af0edeb30802405c1e2bc33ee830
LSevilla <leonardosevillaanda@gmail.com>
2026-08-03 14:16:24 -0400

    Create  firebase-config.js

---
+++ src/infrastructure/firebase/firebase-config.js
@@ -0,0 +1,13 @@
+// src/infrastructure/firebase/firebase-config.js
+export const firebaseConfig = {
+  firebaseOptions: {
+    apiKey: 'AIzaSyDihNGRRQ4SX6Af1fk0yDgCEE1CUog5lmU',
+    authDomain: 'aporte-compartido.firebaseapp.com',
+    projectId: 'aporte-compartido',
+    storageBucket: 'aporte-compartido.firebasestorage.app',
+    messagingSenderId: '601400548392',
+    appId: '1:601400548392:web:66eb36717e655a4e90691d',
+  },
+  useEmulator: false,
+  emulatorUrl: 'http://localhost:9099',
+};
